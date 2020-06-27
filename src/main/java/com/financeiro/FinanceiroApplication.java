abef84d51489037a1945d5fa36b877525a81a90a
johnatanbrayan <johnatanbrayan1@gmail.com>
2020-06-27 13:28:11 -0300

    Connection e configure database mysql

--- src/main/java/com/financeiro/FinanceiroApplication.java
+++ src/main/java/com/financeiro/FinanceiroApplication.java
@@ -1,4 +1,4 @@
-package com.financeiro.financeiro;
+package com.financeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
